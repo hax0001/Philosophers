134df0d68c8623be21fb122250a1262cea3d26e0
Nour Eddine Ait Bouguarri <nait-bou@c4r8p8.1337.ma>
2024-07-22 18:52:17 +0100

    start

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 17:38:17 by nait-bou          #+#    #+#             */
+/*   Updated: 2024/07/22 18:51:29 by nait-bou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
